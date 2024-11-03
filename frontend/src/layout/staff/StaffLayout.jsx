1dd8b68c369238d4ef24819a1b7c4825bd397035
__.anle.__ <147271364+anle404@users.noreply.github.com>
2024-11-04 00:34:27 +0700

    abcxyz

--- frontend/src/layout/staff/StaffLayout.jsx
+++ frontend/src/layout/staff/StaffLayout.jsx
@@ -1,6 +1,9 @@
 import { Outlet } from "react-router-dom";
+import { setCookie, getCookie, deleteCookie } from "../../components/Methods";
 
 export default function StaffLayout() {
+    setCookie("name", "Anle", 1);
+    
     return (
         <div>
             <div>Staff Page</div>
