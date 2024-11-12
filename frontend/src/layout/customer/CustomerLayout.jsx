828765446fd44354ffd3aa6014f78e6587f68776
PancakesLmao <phucthin29@gmail.com>
2024-11-12 13:59:16 +0700

    updated footer

--- frontend/src/layout/customer/CustomerLayout.jsx
+++ frontend/src/layout/customer/CustomerLayout.jsx
@@ -1,6 +1,30 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/customer/header";
 import Footer from "../../components/customer/footer";
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Mireille';
+    src: url('../../fonts/Demo-Mireille/Fontspring-DEMO-mireille-medium.otf') format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  body {
+    font-family: 'Mireille', sans-serif;
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <CustomerLayout />
+    </>
+  );
+}
+
 
 export default function CustomerLayout() {
   return (
