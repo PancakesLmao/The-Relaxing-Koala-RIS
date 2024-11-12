7abdcd1d6720b1342b3d8209f2e7efaeb173adef
__.anle.__ <147271364+anle404@users.noreply.github.com>
2024-11-13 01:26:16 +0700

    Reservation (Ongoing)

--- frontend/src/layout/staff/HistoryLayout.jsx
+++ frontend/src/layout/staff/HistoryLayout.jsx
@@ -1,11 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 
 export default function HistoryLayout() {
+    const location = useLocation().pathname.split('/').reverse()[0]
+    console.log(location)
     return (
         <div className="pl-[1vw]">
             <div className="flex pt-[1.2vw] justify-between h-[10vh]">
-                <div className="text-[2.4vw] font-medium pt-[0.6vw]">
-                    History
+                <div className="flex items-center pt-[0.6vw] gap-[1.5vw]">
+                    {location === "order-history" ? 
+                        <p className="text-[2.4vw] font-medium">Order History</p>
+                    :
+                        <p className="text-[2.4vw] font-medium">Receipt History</p>}  
+                    <NavLink to="./order-history" className="text-center staff-history-navitems">
+                        <button className="text-[1.4vw] font-medium bg-honeydew py-[0.2vw] w-[8vw] rounded-[0.6vw]">
+                            Order
+                        </button>
+                    </NavLink>
+                    <NavLink to="./receipt-history" className="text-center staff-history-navitems">
+                        <button className="text-[1.4vw] font-medium bg-honeydew py-[0.2vw] w-[8vw] rounded-[0.6vw]">
+                            Receipts
+                        </button>
+                    </NavLink>
                 </div>
                 <i className="material-symbols-outlined text-[4vw] act-circ">account_circle</i>
             </div>
--- frontend/src/pages/staff/Reservation.jsx
+++ frontend/src/pages/staff/Reservation.jsx
@@ -1,6 +1,61 @@
+import { useEffect, useRef, useState } from "react"
+
 export default function Reservation() {
+    const [selectedOption, setSelectedOption] = useState("name");
+    const [reservations, setReservations] = useState([]);
+    const [fetchReserv, setFetchReserv] = useState(true);
+    const searchValue = useRef("");
+
+    useEffect(() => {
+        if (fetchReserv) {
+            fetch("http://127.0.0.1:8000/reservations/get-all-reservations").then(
+                response => {
+                    if (response.status === 200) {
+                        return response.json();
+                    }
+                }
+            ).then(data => {
+                setReservations(data)
+            })
+        }
+    }, [fetchReserv])
+
+    function searchReservation(event) {
+        event.preventDefault()
+        fetch(`http://127.0.0.1:8000/reservations/get-reservations-from-${selectedOption}/${searchValue.current.value}`).then(
+            response => {
+                if (response.status === 200) {
+                    return response.json()
+                }
+
+                return Response.reject()
+            }
+        ).then(data => {
+            setReservations(data)
+        }).catch(() => {
+            setReservations([])
+        })
+    }
+
+    // function deleteReservation() {
+        
+    // }
+
+    function checkInReservation(reservation) {
+        fetch("http://127.0.0.1:8000/tables/add-order", {
+            method: 'PATCH',
+            headers: {
+                'Context-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                table_number: reservation.table_number,
+                customer_name: reservation.customer_name
+            })
+        })
+    }
+
     return (
-        <div className="pl-[1vw]">
+        <div className="px-[1vw]">
             <div className="flex pt-[1.2vw] justify-between h-[10vh]">
                 <div className="text-[2.4vw] font-medium pt-[0.6vw]">
                     Reservation
@@ -8,7 +63,62 @@ export default function Reservation() {
                 <i className="material-symbols-outlined text-[4vw] act-circ">account_circle</i>
             </div>
             <div className="h-[0.2vw] w-[100%] bg-gunmetal"></div>
-                    
+            <form onSubmit={(event) => searchReservation(event)}>
+                <div className="flex font-medium items-center gap-[5vw] py-[0.6vw]">
+                    <p className="text-[1.8vw]">Search By:</p>
+                    <div className="flex gap-[5vw] text-[1.7vw]">
+                        <div className="flex items-center gap-[0.3vw]">
+                            <input type="radio" name="search-type" value="name" className="w-[1.3vw] h-[1.3vw]" defaultChecked={selectedOption === "name"} onChange={() => setSelectedOption("name")}/>
+                            <label htmlFor="name" onClick={() => setSelectedOption("name")}>Customer's Name</label>
+                        </div>
+                        <div className="flex items-center gap-[0.3vw]">
+                            <input type="radio" name="search-type" value="phone" className="w-[1.3vw] h-[1.3vw]" defaultChecked={selectedOption === "phone"} onChange={() => setSelectedOption("phone")}/>
+                            <label htmlFor="phone" onClick={() => setSelectedOption("phone")}>Phone Number</label>
+                        </div>
+                        <div className="flex items-center gap-[0.3vw]">
+                            <input type="radio" name="search-type" value="table" className="w-[1.3vw] h-[1.3vw]" defaultChecked={selectedOption === "table"} onChange={() => setSelectedOption("table")}/>
+                            <label htmlFor="table" onClick={() => setSelectedOption("table")}>Table</label>
+                        </div>                        
+                    </div>
+                </div>
+                <div className="flex items-stretch gap-[1vw]">
+                    <input type="text" id="search-value" name="search-value" placeholder="Search..." ref={searchValue} className="flex-1 border-[0.1vw] border-gunmetal pl-[0.5vw] text-[1.3vw]"></input>
+                    <button className="w-[5vw] bg-gunmetal">
+                        <i className="ri-search-line text-honeydew text-[1.7vw]"></i>
+                    </button>
+                </div>
+            </form>
+            <div className="w-[100%] h-[0.15vw] bg-gunmetal mt-[2vw]"></div>
+            <div className="flex gap-[2.5vw] font-medium text-[1.5vw] ml-[2vw] py-[0.7vw]">
+                <p className="w-[2vw]">ID</p>
+                <p className="w-[18vw]">Name</p>
+                <p className="w-[9vw]">Phone</p>
+                <p className="w-[6vw] text-center">People</p>
+                <p className="w-[15vw]">Date and Time</p>
+                <p className="w-[4vw] text-center">Table</p>
+            </div>
+            {reservations.map((reservation, index) => (
+                <div key={index} className="bg-antiflash-white w-[100%] flex flex-col border-[0.1vw] border-silver mb-[1.5vw]">
+                    <div className="flex justify-between mx-[2vw] py-[1.3vw]">
+                        <div className="flex gap-[2.5vw] font-light text-[1.5vw] items-center">
+                            <p className="w-[2vw]">{reservation.reservation_id}</p>
+                            <p className="w-[18vw]">{reservation.customer_name}</p>
+                            <p className="w-[9vw]">{reservation.customer_phone}</p>
+                            <p className="w-[6vw] text-center">{reservation.number_of_people}</p>
+                            <p className="w-[15vw]">{reservation.date_reserved}</p>
+                            <p className="w-[4vw] text-center">{reservation.table_number}</p>
+                        </div>
+                        <div className="flex items-center gap-[2.5vw]">
+                            <i className="ri-close-line leading-[1] text-[2.5vw] p-[0.2vw] bg-red text-white rounded-[0.5vw] cursor-pointer"></i>
+                            <i className="ri-check-line leading-[1] text-[2.5vw] p-[0.2vw] bg-lime-green text-white rounded-[0.5vw] cursor-pointer" 
+                                onClick={() => checkInReservation(reservation)}></i>
+                        </div>            
+                    </div>
+                    <div className="bg-silver flex items-center w-[100%] h-[3vw]">
+                        <p className="ml-[2vw] font-light text-[1.5vw]">Notes: {reservation.notes}</p>
+                    </div>
+                </div>
+            ))}               
         </div>
     )
 }
